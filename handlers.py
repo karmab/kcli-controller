74e2a616cbeb1f80b25c67565b304bca89026832
karmab <karimboumedhel@gmail.com>
2019-10-02 22:21:55 +0200

    simpler crds and update vm support

--- handlers.py
+++ handlers.py
@@ -2,13 +2,14 @@
 
 import kopf
 from kvirt.config import Kconfig
+from kvirt import common
 import os
 import shutil
 
 DOMAIN = "kcli.karmalabs.local"
 
 
-def process_vm(name, namespace, spec, operation='create'):
+def process_vm(name, spec, operation='create'):
     config = Kconfig(quiet=True)
     exists = config.k.exists(name)
     if operation == "delete" and exists:
@@ -21,7 +22,12 @@ def process_vm(name, namespace, spec, operation='create'):
         return
     if operation == "create" and not exists:
         profile = spec.get("profile")
-        overrides = spec.get("parameters", {})
+        if profile is None:
+            if 'template' in spec:
+                profile = spec['template']
+            else:
+                profile = name
+        overrides = spec
         print("Creating: %s" % name)
         if profile is not None:
             result = config.create_vm(name, profile, overrides=overrides)
@@ -32,7 +38,7 @@ def process_vm(name, namespace, spec, operation='create'):
         return
 
 
-def process_plan(plan, namespace, spec, operation='create'):
+def process_plan(plan, spec, operation='create'):
     inputstring = spec.get('plan')
     if inputstring is None:
         print("Failure : %s not created because of missing plan spec")
@@ -57,6 +63,53 @@ def process_plan(plan, namespace, spec, operation='create'):
         return
 
 
+def update(name, spec):
+    config = Kconfig(quiet=True)
+    k = config.k
+    info = k.info(name)
+    for arg in spec:
+        if arg == 'plan':
+            plan = spec[arg]
+            if info['plan'] != plan:
+                common.pprint("Updating plan of vm %s to %s..." % (name, plan))
+                k.update_metadata(name, 'plan', plan)
+        if arg == 'memory':
+            memory = spec[arg]
+            if info['memory'] != memory:
+                common.pprint("Updating memory of vm %s to %s..." % (name, memory))
+                k.update_memory(name, memory)
+        if arg == 'numcpus':
+            numcpus = spec[arg]
+            if info['numcpus'] != numcpus:
+                common.pprint("Updating numcpus of vm %s to %s..." % (name, numcpus))
+                k.update_cpus(name, numcpus)
+        if arg == 'autostart':
+            autostart = spec[arg]
+            common.pprint("Setting autostart for vm %s to %s..." % (name, autostart))
+            k.update_start(name, start=autostart)
+        if arg == 'information':
+            information = spec[arg]
+            common.pprint("Setting information for vm %s..." % name)
+            k.update_information(name, information)
+        if arg == 'iso':
+            iso = spec[arg]
+            common.pprint("Switching iso for vm %s to %s..." % (name, iso))
+            k.update_iso(name, iso)
+        if arg == 'flavor':
+            flavor = spec[arg]
+            if info['flavor'] != flavor:
+                common.pprint("Updating flavor of vm %s to %s..." % (name, flavor))
+                k.update_flavor(name, flavor)
+        if arg == 'start':
+            start = spec[arg]
+            if start:
+                common.pprint("Starting vm %s..." % name)
+                k.start(name)
+            else:
+                common.pprint("Stopping vm %s..." % name)
+                k.stop(name)
+
+
 homedir = os.path.expanduser("~")
 cmdir = "%s/.kcli_cm" % homedir
 kclidir = "%s/.kcli" % homedir
@@ -71,7 +124,7 @@ def create_vm(meta, spec, status, namespace, logger, **kwargs):
     exist = status.get('create_vm', None)
     if exist is None:
         print("Handling %s on vm %s" % (operation, name))
-        process_vm(name, namespace, spec, operation=operation)
+        process_vm(name, spec, operation=operation)
         return {'exist': True}
 
 
@@ -82,7 +135,15 @@ def delete_vm(meta, spec, namespace, logger, **kwargs):
     keep = spec.get("keep", False)
     if not keep:
         print("Handling %s on vm %s" % (operation, name))
-        process_vm(name, namespace, spec, operation=operation)
+        process_vm(name, spec, operation=operation)
+
+
+@kopf.on.update('kcli.karmalabs.local', 'v1', 'vms')
+def update_vm(meta, spec, namespace, logger, **kwargs):
+    operation = 'update'
+    name = meta.get('name')
+    print("Handling %s on vm %s" % (operation, name))
+    update(name, spec)
 
 
 @kopf.on.create('kcli.karmalabs.local', 'v1', 'plans')
@@ -90,7 +151,7 @@ def create_plan(meta, spec, status, namespace, logger, **kwargs):
     operation = 'create'
     name = meta.get('name')
     print("Handling %s on plan %s" % (operation, name))
-    process_plan(name, namespace, spec, operation=operation)
+    process_plan(name, spec, operation=operation)
     return {'exist': True}
 
 
@@ -100,5 +161,5 @@ def delete_plan(meta, spec, namespace, logger, **kwargs):
     name = meta.get('name')
     if spec.get('plan') is not None:
         print("Handling %s on plan %s" % (operation, name))
-        process_plan(name, namespace, spec, operation=operation)
+        process_plan(name, spec, operation=operation)
         return {'exist': True}
